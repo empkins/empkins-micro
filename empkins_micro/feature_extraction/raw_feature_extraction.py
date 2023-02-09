f2b6428a9361b8d5822d59b7a188eb3615af5f82
Robert Richer <robert.richer@fau.de>
2023-02-09 16:38:14 +0100

    formatting

--- empkins_micro/feature_extraction/raw_feature_extraction.py
+++ empkins_micro/feature_extraction/raw_feature_extraction.py
@@ -1,22 +1,21 @@
-import pandas as pd
-import numpy as np
-from pathlib import Path
-from typing import Tuple, Optional
-import tarfile
-import io
 import os
 import shutil
+import tarfile
+from pathlib import Path
+from typing import Optional
 
-from empkins_micro.utils._types import path_t
-from empkins_micro.feature_extraction.acoustic.shimmer import calc_shimmer
-from empkins_micro.feature_extraction.acoustic.jitter import calc_jitter
-from empkins_micro.feature_extraction.acoustic.glottal_noise import calc_gne
-from empkins_micro.feature_extraction.movement.eyeblink import binarize_eyeblink
+import pandas as pd
 from empkins_io.datasets.d03.macro_prestudy.helper import build_opendbm_tarfile_path, build_opendbm_raw_data_path
+
+from empkins_micro.feature_extraction.acoustic.glottal_noise import calc_gne
 from empkins_micro.feature_extraction.acoustic.helper import process_segment_pitch
+from empkins_micro.feature_extraction.acoustic.jitter import calc_jitter
+from empkins_micro.feature_extraction.acoustic.shimmer import calc_shimmer
+from empkins_micro.feature_extraction.movement.eyeblink import binarize_eyeblink
+from empkins_micro.utils._types import path_t
 
 
-class RawFeatureExtraction():
+class RawFeatureExtraction:
     _base_path: path_t
     _subject_id: str
     _condition: str
@@ -24,13 +23,15 @@ class RawFeatureExtraction():
     _audio_path: path_t
     _df_eyeblink: pd.DataFrame
 
-    def __init__(self,
-                 base_path: path_t,
-                 subject_id: str,
-                 condition: str,
-                 df_pitch: Optional[pd.DataFrame] = None,
-                 audio_path: Optional[path_t] = "",
-                 df_eyeblink: Optional[pd.DataFrame] = None):
+    def __init__(
+        self,
+        base_path: path_t,
+        subject_id: str,
+        condition: str,
+        df_pitch: Optional[pd.DataFrame] = None,
+        audio_path: Optional[path_t] = "",
+        df_eyeblink: Optional[pd.DataFrame] = None,
+    ):
 
         self._base_path = base_path
         self._subject_id = subject_id
@@ -45,17 +46,22 @@ class RawFeatureExtraction():
     def write_file(self, base_path: path_t, subject_id: str, condition: str):
 
         tarfile_path = build_opendbm_tarfile_path(base_path=base_path, subject_id=subject_id, condition=condition)
-        tarfile_path_new = build_opendbm_tarfile_path(base_path=base_path, subject_id=subject_id, condition=condition,
-                                                      new=True)
+        tarfile_path_new = build_opendbm_tarfile_path(
+            base_path=base_path, subject_id=subject_id, condition=condition, new=True
+        )
 
-        jitter_path = build_opendbm_raw_data_path(subject_id=self._subject_id, condition=self._condition,
-                                                  group="acoustic", subgroup="jitter_recomputed")[0]
-        shimmer_path = build_opendbm_raw_data_path(subject_id=self._subject_id, condition=self._condition,
-                                                   group="acoustic", subgroup="shimmer_recomputed")[0]
-        gne_path = build_opendbm_raw_data_path(subject_id=self._subject_id, condition=self._condition,
-                                               group="acoustic", subgroup="gne_recomputed")[0]
-        eyeblink_path = build_opendbm_raw_data_path(subject_id=self._subject_id, condition=self._condition,
-                                                    group="movement", subgroup="eyeblink_binarized")[0]
+        jitter_path = build_opendbm_raw_data_path(
+            subject_id=self._subject_id, condition=self._condition, group="acoustic", subgroup="jitter_recomputed"
+        )[0]
+        shimmer_path = build_opendbm_raw_data_path(
+            subject_id=self._subject_id, condition=self._condition, group="acoustic", subgroup="shimmer_recomputed"
+        )[0]
+        gne_path = build_opendbm_raw_data_path(
+            subject_id=self._subject_id, condition=self._condition, group="acoustic", subgroup="gne_recomputed"
+        )[0]
+        eyeblink_path = build_opendbm_raw_data_path(
+            subject_id=self._subject_id, condition=self._condition, group="movement", subgroup="eyeblink_binarized"
+        )[0]
 
         tmp_path = self.get_temporary_path()
 
@@ -76,7 +82,6 @@ class RawFeatureExtraction():
                 tar.add(tmp_path.joinpath("eyeblink.csv"), arcname=eyeblink_path)
 
         shutil.rmtree(tmp_path)
-
 
     def feature_extraction(self):
 
@@ -99,15 +104,14 @@ class RawFeatureExtraction():
             df_gne.to_csv(tmp_path.joinpath("gne.csv"), index=False)
 
         else:
-            raise print('pitch dataframe (df_pitch) is not initialized')
+            raise print("pitch dataframe (df_pitch) is not initialized")
 
         if self._df_eyeblink is not None:
             df_eyeblink = binarize_eyeblink(self._df_eyeblink)
             df_eyeblink.to_csv(tmp_path.joinpath("eyeblink.csv"), index=False)
 
         else:
-            raise print('eyeblink dataframe is not initialized')
+            raise print("eyeblink dataframe is not initialized")
 
         if self._df_pitch is not None or self._df_eyeblink is not None:
             self.write_file(self._base_path.joinpath("data_per_subject"), self._subject_id, self._condition)
-
