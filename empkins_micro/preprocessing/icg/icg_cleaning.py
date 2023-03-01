2a64f15a30701305e00b4f5befe5da3fa3629700
Ulla Sternemann <ulla.sternemann@fau.de>
2023-03-01 17:11:31 +0100

    function for icg cleaning using butterworth filter

---
+++ empkins_micro/preprocessing/icg/icg_cleaning.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from scipy import signal
+
+
+def clean_icg_deriv(raw_signal: pd.Series, sampling_rate_hz: int) -> pd.DataFrame:
+    """function which cleans ICG dZ/dt signal using butterworth filtering
+
+    (low cutoff: 0.5 Hz, high cutoff: 25 Hz, see Forouzanfar 2019)
+
+    Args:
+        raw_signal: pd.Series containing the raw dZ/dt ICG signal
+        sampling_rate_hz: sampling rate of ICG dZ/dt signal in hz
+    Returns:
+        clean_signal: pd.DataFrame containing filtered signal
+    """
+    
+    clean_signal = pd.DataFrame(index=raw_signal.index)
+    sos = signal.butter(N=4, Wn=[0.5, 25], btype="bandpass", output="sos", fs=sampling_rate_hz)
+    clean_signal["icg_der_clean"] = signal.sosfiltfilt(sos, raw_signal)
+    return clean_signal
+
