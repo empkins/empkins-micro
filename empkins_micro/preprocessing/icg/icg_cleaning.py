baa9402203b9f55f093e3d8c46c4545f736de0c1
Ulla Sternemann <ulla.sternemann@fau.de>
2023-03-02 10:54:11 +0100

    changed return value to pd series

--- empkins_micro/preprocessing/icg/icg_cleaning.py
+++ empkins_micro/preprocessing/icg/icg_cleaning.py
@@ -2,7 +2,7 @@ import pandas as pd
 from scipy import signal
 
 
-def clean_icg_deriv(raw_signal: pd.Series, sampling_rate_hz: int) -> pd.DataFrame:
+def clean_icg_deriv(raw_signal: pd.Series, sampling_rate_hz: int) -> pd.Series:
     """function which cleans ICG dZ/dt signal using butterworth filtering
 
     (low cutoff: 0.5 Hz, high cutoff: 25 Hz, see Forouzanfar 2019)
@@ -14,8 +14,8 @@ def clean_icg_deriv(raw_signal: pd.Series, sampling_rate_hz: int) -> pd.DataFram
         clean_signal: pd.DataFrame containing filtered signal
     """
 
-    clean_signal = pd.DataFrame(index=raw_signal.index)
     sos = signal.butter(N=4, Wn=[0.5, 25], btype="bandpass", output="sos", fs=sampling_rate_hz)
-    clean_signal["icg_der_clean"] = signal.sosfiltfilt(sos, raw_signal)
+    clean_signal = signal.sosfiltfilt(sos, raw_signal)
+    clean_signal = pd.Series(clean_signal, index=raw_signal.index)
     return clean_signal
 
