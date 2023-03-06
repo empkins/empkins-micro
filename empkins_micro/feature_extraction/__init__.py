945a67f7dbd2f83c2442c3b968b1c674b245c73c
Ulla Sternemann <ulla.sternemann@fau.de>
2023-03-06 12:37:28 +0100

    added pep

--- empkins_micro/feature_extraction/__init__.py
+++ empkins_micro/feature_extraction/__init__.py
@@ -3,3 +3,4 @@ from empkins_micro.feature_extraction import movement
 from empkins_micro.feature_extraction import raw_feature_extraction
 from empkins_micro.feature_extraction import utils
 from empkins_micro.feature_extraction import resources
+from empkins_micro.feature_extraction import pep
