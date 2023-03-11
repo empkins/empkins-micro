3d1ebcd0126a2d1ee55d3fda9a9fe322b3ed06cb
Ulla Sternemann <ulla.sternemann@fau.de>
2023-03-11 14:11:07 +0100

    removed static function which matches fiducial points to corresponding heartbeats, because this will look different for each kind of fiducial point, so it is implemented within the subclasses for specific point extraction

--- empkins_micro/feature_extraction/pep/algorithms/base_extraction.py
+++ empkins_micro/feature_extraction/pep/algorithms/base_extraction.py
@@ -1,8 +1,7 @@
 from abc import abstractmethod
 
 import pandas as pd
-from tpcp import Algorithm, Parameter, make_action_safe
-from typing import List
+from tpcp import Algorithm, make_action_safe
 
 
 class BaseExtraction(Algorithm):
@@ -24,28 +23,3 @@ class BaseExtraction(Algorithm):
         """function which extracts specific fiducial points from cleaned signal, implementation within subclasses"""
 
         pass
-
-    @staticmethod
-    def match_points_heartbeats(points: List[int], heartbeats: pd.DataFrame) -> pd.DataFrame:
-        """matches the given points to the corresponding heartbeats
-
-        (such that returned DataFrame's format matches heartbeats df)
-
-        Args:
-            points: list of fiducial points of signal (samples)
-            heartbeats: pd.DataFrame as returned by HeartBeatExtraction
-
-        Returns:
-            pd.DataFrame with point locations saved in row of associated heartbeat
-        """
-
-        points_heartbeats = pd.DataFrame(index=heartbeats.index, columns=["point_sample"])  # create result df
-
-        # TODO evt check, ob länge gleich ist, ob punkt übrig ist, ob zB q-peak vor r liegt oder sowas (warnings für sanity checks)?
-        for p in points:
-            # find heartbeat to which point belongs and write point into corresponding row
-            idx = heartbeats.loc[(heartbeats["start_sample"] < p) & (p < heartbeats["end_sample"])].index
-            points_heartbeats["point_sample"].loc[idx] = p
-
-        return points_heartbeats
-
