41964d706d5a59032090173ad7c8b65ccfdb2628
Ulla Sternemann <ulla.sternemann@fau.de>
2023-04-06 18:22:35 +0200

    added norm calculation function

--- empkins_micro/preprocessing/scg/scg_cleaning.py
+++ empkins_micro/preprocessing/scg/scg_cleaning.py
@@ -1,6 +1,7 @@
 from typing import Union, Optional
 
 import pandas as pd
+from numpy.linalg import norm
 from scipy import signal
 
 
@@ -24,17 +25,36 @@ def clean_scg(raw_signal: Union[pd.Series, pd.DataFrame], sampling_rate_hz: int,
         sos = signal.butter(N=4, Wn=[5, 40], btype="bandpass", output="sos", fs=sampling_rate_hz)
 
         if isinstance(raw_signal, pd.Series):
-            filtered_signal = signal.sosfiltfilt(sos, raw_signal)
-            clean_signal = pd.Series(filtered_signal, index=raw_signal.index, name=raw_signal.name)
+            filtered = signal.sosfiltfilt(sos, raw_signal)
+            clean_signal = pd.Series(filtered, index=raw_signal.index, name=raw_signal.name)
             return clean_signal
 
         elif isinstance(raw_signal, pd.DataFrame):
-            #clean_signal = pd.DataFrame(columns=raw_signal.columns, index=raw_signal.index)
-            filtered_signals = raw_signal.apply(lambda x: signal.sosfiltfilt(sos, x), axis=0)
-            return filtered_signals
+            clean_signal = raw_signal.apply(lambda x: signal.sosfiltfilt(sos, x), axis=0)
+            return clean_signal
 
         else:
             raise ValueError("Input (raw_signal) needs to be either pd.Series or pd.DataFrame")
 
     else:
         raise ValueError("Not implemented yet!")
+
+
+def scg_norm(xyz_acc_signals: pd.DataFrame) -> pd.Series:
+    """function to clean SCG signal
+
+    Args:
+        xyz_acc_signals: pd.DataFrame containing 3 columns for x, y, z SCG components (acc), should be cleaned previously
+
+    Returns:
+        signal_norm: pd.Series containing the L2-norm of the 3 components
+    """
+
+    # check if raw_signal contains 3 columns
+    if xyz_acc_signals.shape[1] != 3:
+        raise ValueError(
+            f"The input signal must contain 3 columns (instead of {xyz_acc_signals.shape[1]}) for x, y, z components!")
+
+    calculated_norm = norm(xyz_acc_signals, axis=1)
+    signal_norm = pd.Series(calculated_norm, index=xyz_acc_signals.index, name="acc_norm")
+    return signal_norm
