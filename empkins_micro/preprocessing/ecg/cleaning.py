fa475e7a6a2070b67799fd7cdb625b75cba2258b
Ulla Sternemann <ulla.sternemann@fau.de>
2023-04-12 18:10:41 +0200

    changed default method to biosppy

--- empkins_micro/preprocessing/ecg/cleaning.py
+++ empkins_micro/preprocessing/ecg/cleaning.py
@@ -3,21 +3,23 @@ import pandas as pd
 from typing import Optional
 
 
-def clean_ecg(raw_signal: pd.Series, sampling_rate_hz: int, method: Optional[str] = "neurokit") -> pd.Series:
-    """function to clean ECG signals using neurokit's ecg_clean function
+def clean_ecg(raw_signal: pd.Series, sampling_rate_hz: int, method: Optional[str] = "biosppy") -> pd.Series:
+    """function to clean ECG signals using neurokit's biosppy ecg_clean function
 
     Args:
         raw_signal: pd.Series containing the raw ECG signal
         sampling_rate_hz: sampling rate of the ECG signal in hz
-        method: cleaning method (default is "neurokit"), can be any of the options of neurokit's ecg_clean function
+        method: cleaning method (default is "biosppy"), can be any either "neurokit" or "biosppy"
 
     Returns:
         clean_signal: pd.DataFrame containing filtered signal while keeping index of input signal
     """
 
-    # TODO: add other cleaning methods
-
     if method == "neurokit":
+        clean_signal = nk.ecg_clean(raw_signal, sampling_rate=sampling_rate_hz, method=method)
+        clean_signal = pd.Series(clean_signal, index=raw_signal.index, name="ecg")
+
+    elif method == "biosppy":
         clean_signal = nk.ecg_clean(raw_signal, sampling_rate=sampling_rate_hz, method=method)
         clean_signal = pd.Series(clean_signal, index=raw_signal.index, name="ecg")
 
